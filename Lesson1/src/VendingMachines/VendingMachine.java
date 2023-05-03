f48e2dc2782715ae95e894261fe4e2e168d4f03b
MaryanaAm <119092185+MaryanaAm@users.noreply.github.com>
2023-05-03 03:49:45 +0300

    Update VendingMachine.java

--- Lesson1/src/VendingMachines/VendingMachine.java
+++ Lesson1/src/VendingMachines/VendingMachine.java
@@ -9,7 +9,6 @@ public class VendingMachine {
     private int volume;
     private List<Product> products;// = new ArrayList<Product>();
     private List<String> workLog;// = new ArrayList<String>();
-    private Double money;
 
     public VendingMachine(int volume){
         this.volume = volume;
@@ -46,15 +45,4 @@ public class VendingMachine {
     {
         return products;
     }
-    public String buy(Product product, Double money) {
-        if(money >= product.getPrice()) {
-            Double change = money - product.getPrice();
-            money = 0.0;
-            return "Продукт: " + product.getName() + ". Сдача: " + change;
-        }
-        else{
-            return "Недостаточно денег для покупки";
-        }
-    }
-
 }
